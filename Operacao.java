7407439e610b8376e2a490ef38b661500bd3cd89
soares <gysoares99@gmail.com>
2026-09-07 12:30:56 -0300

    Gy; 07/09
    Modificação:Nem uma, apenas comentei todo  o codigo feito até agora, tirando a parte da fila

--- Operacao.java
+++ Operacao.java
@@ -1,11 +1,11 @@
-//Uma classe como Operacao poderá armazenar
-public class Operacao {
- public String tipo;
- public Solicitacao solicitacao;
+//class com as operações
+public class Operacao {//Inicio class Operacao
+ public String tipo;     //tipo até agora fica em CADASTRO, logo que cadadastramos uma nova_solicitação o tipo CADASTRO vem para cá
+ public Solicitacao solicitacao;  //a nova_solicitação vem para cá
 
-    public Operacao(String tipo, Solicitacao solicitacao) {
-        this.tipo = tipo;
-        this.solicitacao = solicitacao;
-    }
+    public Operacao(String tipo, Solicitacao solicitacao) { //iniicio metodo Operação
+        this.tipo = tipo; //coloca o tipo dentro do tipo
+        this.solicitacao = solicitacao;  //coloca a nova_solicitação dentro da solicitacao
+    }//fim metodo Operação
 
-}
+}//fim class Operação
--- Solicitacao.java
+++ Solicitacao.java
@@ -16,7 +16,7 @@
     • CANCELADA
 */
 
-public class Solicitacao {
+public class Solicitacao {//inicio class solicitação
     public int codigo;
     public String solicitante;
     public String descricao;
@@ -24,17 +24,18 @@ public class Solicitacao {
     public int prioridade;
     public String status;
 
-    public Solicitacao(int codigo, String solicitante, String descricao, String categoria, int prioridade, String status) {
+    public Solicitacao(int codigo, String solicitante, String descricao, String categoria, int prioridade, String status) {//inicio metodo solicitacao
         this.codigo = codigo;
         this.solicitante = solicitante;
         this.descricao = descricao;
         this.categoria = categoria;
         this.prioridade = prioridade;
         this.status = status;
+        //imprimi a solicitação, como teste para saber que a solicitação foi criada, e que os dados foram passados corretamente, e que o construtor está funcionando
         System.out.printf("\n === Solicitação: ===\nCódigo: %d, Solicitante: %s, Descrição: %s, Categoria: %s, Prioridade: %d, Status: %s\n", codigo, solicitante, descricao, categoria, prioridade, status);
         System.out.printf("\n");
-    }
-}
+    }//fim metodo solicitacao
+}//fim class solicitação
 
 
     
