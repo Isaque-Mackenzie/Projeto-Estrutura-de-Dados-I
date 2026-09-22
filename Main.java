c2ee879944d96acb6997d57eb81876f8b417f55b
Isaque <isaquemackcc@gmail.com>
2026-09-22 20:32:26 -0300

    Clean up comments in Main.java
    Removed unnecessary comments from Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-//classe com nome do arquivo, não esqueça de colocar em maiusculo tanto o nome do arquivo quanto o nome da classe
-    //metodo main, para o java saber por onde começar
-    //Instanciar o objeto menu, para poder chamar o metodo exibir (pois o o metodo exibir esta em outra classe, então precisa do objeto)
-    //usar o metodo para exibir que esta no outro arquivo
-
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
