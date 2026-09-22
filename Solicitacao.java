ca58800caf42e1ba01d180638cc752557f94758e
Isaque <isaquemackcc@gmail.com>
2026-09-22 20:38:23 -0300

    Remove comments from Solicitacao.java
    Removed comments detailing the required fields and states for a request.

--- Solicitacao.java
+++ Solicitacao.java
@@ -1,21 +1,3 @@
-/*
-    Cada solicitação deverá possuir, no mínimo, informações como: 
-    • Código da solicitação 
-    • Nome do solicitante 
-    • Descrição do problema 
-    • Categoria 
-    • Prioridade 
-    • Data/hora de abertura 
-    • Status 
-    • Responsável pelo atendimentop 
-    
-    Possíveis estados: 
-    • AGUARDANDO 
-    • EM_ATENDIMENTO 
-    • CONCLUIDA 
-    • CANCELADA
-*/
-
 import java.time.LocalDateTime;
 
 public class Solicitacao {//inicio class solicitação
